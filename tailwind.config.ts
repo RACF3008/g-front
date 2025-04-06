0a2e9e1deaa57bd90d5d3c7aef4726f916b07692
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2025-04-05 22:23:14 -0600

    Finished first version of common elements. - Created side menu component - Created navbar - Configured color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,33 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          100: "#ccd1d1",
+          200: "#99a3a3",
+          300: "#667474",
+          400: "#334646",
+          500: "#001818",
+          600: "#001313",
+          700: "#000e0e",
+          800: "#000a0a",
+          900: "#000505",
+        },
+        secondary: {
+          100: "#d0fae7",
+          200: "#a1f5ce",
+          300: "#73efb6",
+          400: "#44ea9d",
+          500: "#15e585",
+          600: "#11b76a",
+          700: "#0d8950",
+          800: "#085c35",
+          900: "#042e1b",
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
