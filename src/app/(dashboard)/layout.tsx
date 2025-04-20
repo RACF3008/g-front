7c8ab2426864d2bcd0b450b23458efd2109cd13b
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2025-04-19 20:49:28 -0600

    - Finished main dashboard for admin and operator - Created Hive list - Designed top buttons and inputs - Created Pagination component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,9 @@
-import Menu from '@/components/Menu';
-import Navbar from '@/components/Navbar';
-import Image from 'next/image';
-import Link from 'next/link';
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+const role = "admin";
 
 export default function DashboardLayout({
   children,
@@ -14,16 +16,16 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[10%] lg:w-[19%] xl:w-[14%] p-4 bg-primary-600">
         <Link href="/" className="flex items-center justify-start gap-1">
           <Image src="/logo.png" alt="logo" width={48} height={48} />
-          <span className="hidden lg:block text-greenAccent-200 font-bold text-lg">
+          <span className="hidden lg:block text-white font-bold text-lg">
             GreenHive
           </span>
         </Link>
 
-        <Menu />
+        <Menu role="admin" />
       </div>
 
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[90%] lg:w-[81%] xl:w-[86%] bg-primary-500 overflow-y-scroll">
+      <div className="w-[86%] md:w-[90%] lg:w-[81%] xl:w-[86%] bg-primary-500 overflow-y-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,94 +1,98 @@
-import Link from 'next/link';
-import HomeFilledIcon from '@mui/icons-material/HomeFilled';
-import HiveIcon from '@mui/icons-material/Hive';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import PersonIcon from '@mui/icons-material/Person';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import LogoutIcon from '@mui/icons-material/Logout';
+import Link from "next/link";
+import HomeFilledIcon from "@mui/icons-material/HomeFilled";
+import HiveIcon from "@mui/icons-material/Hive";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const menuItems = [
   {
-    title: 'MENU',
+    title: "MENU",
     items: [
       {
-        label: 'Home',
-        href: '/',
+        label: "Home",
+        href: "/",
         icon: <HomeFilledIcon />,
-        visible: ['admin', 'operator', 'viewer'],
+        visible: ["admin", "operator", "viewer"],
       },
+      // {
+      //   label: 'Productivity',
+      //   href: '/',
+      //   icon: <TrendingUpIcon />,
+      //   visible: ['admin'],
+      // },
+      // {
+      //   label: 'Fincance',
+      //   href: '/',
+      //   icon: <AttachMoneyIcon />,
+      //   visible: ['admin'],
+      // },
       {
-        label: 'Productivity',
-        href: '/',
-        icon: <TrendingUpIcon />,
-        visible: ['admin'],
-      },
-      {
-        label: 'Fincance',
-        href: '/',
-        icon: <AttachMoneyIcon />,
-        visible: ['admin'],
-      },
-      {
-        label: 'Hives',
-        href: '/hives',
+        label: "Hives",
+        href: "/hives",
         icon: <HiveIcon />,
-        visible: ['admin', 'operator', 'viewer'],
+        visible: ["admin", "operator", "viewer"],
       },
-      {
-        label: 'Tasks',
-        href: '/tasks',
-        icon: <AssignmentIcon />,
-        visible: ['admin', 'operator'],
-      },
+      // {
+      //   label: 'Tasks',
+      //   href: '/tasks',
+      //   icon: <AssignmentIcon />,
+      //   visible: ['admin', 'operator'],
+      // },
     ],
   },
   {
-    title: 'OTHERS',
+    title: "OTHERS",
     items: [
+      // {
+      //   label: 'Profile',
+      //   href: '/profile',
+      //   icon: <PersonIcon />,
+      //   visible: ['admin', 'operator'],
+      // },
+      // {
+      //   label: 'Settings',
+      //   href: '/settings',
+      //   icon: <SettingsIcon />,
+      //   visible: ['admin', 'operator'],
+      // },
       {
-        label: 'Profile',
-        href: '/profile',
-        icon: <PersonIcon />,
-        visible: ['admin', 'operator'],
-      },
-      {
-        label: 'Settings',
-        href: '/settings',
-        icon: <SettingsIcon />,
-        visible: ['admin', 'operator'],
-      },
-      {
-        label: 'Logout',
-        href: '/logout',
+        label: "Logout",
+        href: "/logout",
         icon: <LogoutIcon />,
-        visible: ['admin', 'operator', 'viewer'],
+        visible: ["admin", "operator", "viewer"],
       },
     ],
   },
 ];
 
-const Menu = () => {
+type MenuProps = {
+  role: string;
+};
+
+const Menu = ({ role }: MenuProps) => {
   return (
-    <div className="mt-4 text-sm ">
+    <div className="mt-4 text-sm">
       {menuItems.map((section) => (
         <div className="flex flex-col gap-2" key={section.title}>
-          <span className="hidden lg:block text-primary-300 front-light my-4">
+          <span className="hidden lg:block text-primary-300 font-light my-4">
             {section.title}
           </span>
-          <div className="flex flex-col gap-2">
-            {section.items.map((item) => (
+          {section.items.map((item) =>
+            item.visible.includes(role) ? (
               <Link
                 href={item.href}
                 key={item.href}
-                className="flex items-center justify-center lg:justify-start gap-4 py-2 text-primary-100 rounded"
+                className="flex items-center justify-center lg:justify-start gap-4 p-2 text-primary-100 rounded-md hover:bg-primary-400 transition"
               >
                 {item.icon}
                 <span className="hidden lg:block">{item.label}</span>
               </Link>
-            ))}
-          </div>
+            ) : null
+          )}
         </div>
       ))}
     </div>
